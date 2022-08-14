d4c286f74b48845594f87d1e2a12da3f16afd810
Muhammad H. M. Ahmed <husseinphysicist@gmail.com>
2022-08-14 19:52:24 +0200

    submission

--- projects/01_fyyur/starter_code/migrations/versions/d4af4c84cad3_.py
+++ projects/01_fyyur/starter_code/migrations/versions/d4af4c84cad3_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 62823d117e44
+Revision ID: d4af4c84cad3
 Revises: 
-Create Date: 2022-08-14 09:32:34.905950
+Create Date: 2022-08-14 19:47:16.670995
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '62823d117e44'
+revision = 'd4af4c84cad3'
 down_revision = None
 branch_labels = None
 depends_on = None
